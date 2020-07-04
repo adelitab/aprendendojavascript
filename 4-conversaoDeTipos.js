e158b2b9542e820c2ff1f37396a4e96d5f7c13b5
adelitab <56311928+adelitab@users.noreply.github.com>
2020-07-04 08:22:35 -0300

    Conversão de Tipos

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,17 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2020);
+console.log("2" + "2");
+
+//se quiser converter o tipo, utiliza o parse
+
+console.log(parseInt("2") + parseInt("2")); //ele converterá para int e fará a soma.
+//chamado de concatenação desses dois textos.
+
+console.log("10" / "2"); //ele faz automaticamente a conversão implícita
+
+console.log("Adelita" / "2"); //devolverá um NaN = Not a Number
+
+console.log("7" / "2"); //o resultado será com ponto flutuante
+console.log(6.5); // ele imprime 6 5. A vírgula serve para ocasiões especiais
+
