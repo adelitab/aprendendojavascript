22e01979d57e9299d0eedb6f554e086dbe8208c0
adelitab <56311928+adelitab@users.noreply.github.com>
2020-07-04 07:49:31 -0300

    1- Trabalhando com Variáveis

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,17 @@
+console.log("Meu Primeiro Programa: Trabalhando com Variáveis");
+
+/*
+    Para acessar no PowerShell:
+    - Copiar o caminho do arquivo, "C:\Users\x\Desktop\Curso Javascript"
+    - Comando: note .\nomedoarquivo.js
+*/
+
+const idade = 32; //salvando valor dentro de uma variável
+console.log(idade); //Shift+Alt+seta para baixo para copiar a linha
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+//const é para declarar a variável
+const idadeSomada = idade+2;
+console.log(idadeSomada);
