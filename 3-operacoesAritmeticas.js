023ced8a9b7eae4e1f69e9a7c511aa47fbf51891
adelitab <56311928+adelitab@users.noreply.github.com>
2020-07-04 08:11:28 -0300

    Operações Aritméticas

---
+++ 3-operacoesAritmeticas.js
@@ -0,0 +1,7 @@
+console.log("Operações Aritméticas");
+
+console.log(2+2);
+console.log(10 + 8 * 2); //imprimirá 26
+console.log((10 + 8) * 2); //imprimirá 36
+
+console.log("ano" + 2020); //imprimirá ano2020
